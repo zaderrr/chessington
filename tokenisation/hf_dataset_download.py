2531cf9c989597f4b6d0d163cf21a1bd977e1b18
zaderrr <ellisbrown88@gmail.com>
2026-03-18 00:08:17 +0000

    Added min elo filter

--- tokenisation/hf_dataset_download.py
+++ tokenisation/hf_dataset_download.py
@@ -1,29 +1,44 @@
 GAMES_PER_FILE = 5_000_000
+MAX_GAMES = 100_000_000
+
 # Not streaming whilst tokenizing, getting data first
 from datasets import load_dataset
-# Lichess/standard-chess-games
+import argparse
 
-year = "xxxx"
-month = "xx"
+#Input args
+parser = argparse.ArgumentParser(description="Config for filtering Lichess games from hugging face")
+parser.add_argument("--min_elo", required=False, type=int,default=0)
+parser.add_argument("--output", required=True, type=str)
+args = parser.parse_args()
+min_elo = args.min_elo
+output_dir= args.output
 
+#Load dataset
 dataset =  load_dataset(
 "Lichess/standard-chess-games",
-data_dir=f"data/year={year}/month={month}",
 split="train",
 streaming=True,
 ) 
-
-
 file_idx = 0
 count = 0
-f = open(f"data/pgn/lichess_{year}_{month}_{file_idx:03d}.pgn", "w")
+
+f = open(f"{output_dir}/{file_idx:03d}.pgn", "w")
 for game in dataset:
+    #Filter minimum elo
+    black_elo = game["WhiteElo"]
+    white_elo = game["BlackElo"]
+    if white_elo is None or black_elo is None:
+        continue
+    if black_elo < min_elo or white_elo < min_elo:
+       continue 
     f.write(game["movetext"] + "\n\n")
     count += 1
-    print (f"{count} games stored." )
+    #Log progressevery 1000 games
+    if (count % 1000 == 0):
+        print(f"Games: {count} / {MAX_GAMES}")
     if count % GAMES_PER_FILE == 0:
         f.close()
         file_idx += 1
-        f = open(f"data/pgn/lichess_{year}_{month}_{file_idx:03d}.pgn", "w")
+        f = open(f"{output_dir}/{file_idx:03d}.pgn", "w")
 
 f.close()
